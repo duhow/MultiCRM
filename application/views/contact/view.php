317a3175ed6c475374ee162f1362c850fc3b8681
duhow <contacto@duhowpi.net>
2018-06-21 14:53:07 +0200

    Añadir secciones de Tags y Tasks. Fase inicial de codigo javascript para render de Tags.

--- application/views/contact/view.php
+++ application/views/contact/view.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 <main>
 	<div class="row mb-3">
-		<div class="col-xs-12 col-md-4 float-right text-right">
+		<div class="col text-right order-12">
 			<div class="btn-group">
 				<button class="btn btn-outline-secondary">
 					<i class="fas fa-plus"></i>
@@ -14,7 +14,7 @@
 				</button>
 			</div>
 		</div>
-		<div class="col-xs-12 col-md-8 float-left">
+		<div class="col order-4">
 			<div class="btn-group">
 				<button class="btn btn-outline-secondary">
 					<i class="fas fa-caret-left"></i>
@@ -35,7 +35,7 @@
 						<i class="fas fa-pencil-alt"></i>
 					</div>
 				</div>
-				<div class="card-body">
+				<div id="contact-profile" class="card-body">
 					<h5 class="card-title">Nombre Apellido Tal</h5>
 					<ul class="card-text list-unstyled">
 						<li>
@@ -57,24 +57,119 @@
 					</ul>
 				
 				</div>
-				<ul class="list-group list-group-flush list-unstyled">
+				<ul id="contact-sources" class="list-group list-group-flush list-unstyled">
+					<li id="contact-tags" class="list-group-item">
+						<textarea class="form-control d-none" name="tags">Campus Espanyol, Scouting, No interesado, </textarea>
+						<div id="contact-tags-list">
+							No hay etiquetas.
+						</div>
+						<div id="contact-tags-add" class="input-group input-group-sm mt-2 d-none">
+							<input type="text" class="form-control" placeholder="Etiqueta" maxlength="100">
+							<div class="input-group-append">
+								<button class="btn btn-outline-secondary" type="button">
+									<i class="fa fa-plus"></i>
+								</button>
+							</div>
+						</div>
+					</li>
 					<li class="list-group-item">
 						<i class="fa fa-envelope"></i>
 						<span>micorreo@ejemplo.com</span>
 						<span class="badge badge-pill badge-primary float-right">5</span>
-					<li>
+					</li>
 					<li class="list-group-item">
 						<i class="fa fa-mobile-alt"></i>
 						<a href="#">+54 112 745 615</a>
 						<span class="badge badge-pill badge-warning float-right">2</span>
-					<li>
+					</li>
 					<li class="list-group-item">
 						<i class="fa fa-phone"></i>
 						<a href="#">+34 901 247 621</a>
 						<span class="badge badge-pill badge-success float-right">3</span>
-					<li>
+					</li>
 				</ul>
+			</div>
+		</div>
+		<div class="col-xs-12 col-md-4">
+			<div class="card">
+				<div class="card-header">
+					Tareas
+					<div class="float-right">
+						<i class="fas fa-plus"></i>
+					</div>
+				</div>
+				<div id="contact-tasks" class="card-body">
+					<div class="form-check">
+						<input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+						<label class="form-check-label" for="defaultCheck2">
+							 Hacer la tarea esa.
+						</label>
+						<span class="text-muted small">Desde hace 2 semanas</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </main>
+
+<script>
+function renderTags(){
+	var tags = $("textarea[name=tags]").val().split(',');
+	if(tags.length > 0){
+		$("#contact-tags-list").empty();
+		var colors = ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'];
+		tags.forEach(function(t){
+			if(t.trim().length <= 1){ return; }
+			var tag = $("<a></a>");
+			var color = (t.trim().length % colors.length);
+			tag.addClass("badge badge-" + colors[color]);
+			// warning and dark
+			if(color != 4 && color != 6){
+				tag.addClass("text-white");
+			}
+			tag.text(t.trim());
+			$("#contact-tags-list").append(tag);
+			$("#contact-tags-list").append(" ");
+		});
+	}
+};
+
+$(function(){
+	$("textarea[name=tags]").change( function(){ renderTags() } );
+	renderTags();
+
+	$("#contact-tags").on('dblclick', function(e){
+		// Only on div, not tags.
+		if(e.target.tagName === "A"){ return; }
+		$("#contact-tags-add").toggleClass("d-none");
+		$("#contact-tags-add input").focus();
+	});
+
+	$('body').on('dblclick', "#contact-tags-list a", function(e){
+		var tag = $(this).text();
+		$("#contact-tags-add input").val(tag);
+		var tags = $("textarea[name=tags]").val();
+		tags = tags.replace(tag + ", ", "");
+		console.log(tags);
+		$("textarea[name=tags]").val(tags);
+		$("textarea[name=tags]").html(tags);
+		renderTags();
+	});
+
+	$("#contact-tags-add input").on('keypress', function(e){
+		// Si pulsa Enter, 
+		// $("#contact-tags-add button").trigger('click');
+	});
+	$("#contact-tags-add button").on('click', function(e){
+		var tag = $("#contact-tags-add input").val().trim();
+		if(tag.length < 1){ return; }
+		var tags = $("textarea[name=tags]").val();
+		if(tags.toLowerCase().indexOf( tag.toLowerCase() ) >= 0){ return; }
+		tags += tag + ", ";
+		$("textarea[name=tags]").val(tags);
+		$("textarea[name=tags]").html(tags);
+		$("#contact-tags-add input").val("");
+		renderTags();
+	});
+});
+</script>
